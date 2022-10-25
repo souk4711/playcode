c2b434cc81dc4f228668065882a70fbb596b1d8e
John Doe <johndoe@example.com>
2022-10-26 00:10:04 +0800

    feat: Support change theme

--- app/javascript/components/AppHeader.vue
+++ app/javascript/components/AppHeader.vue
@@ -1,7 +1,45 @@
 <script setup lang="ts">
-import { LanguageIcon } from '@heroicons/vue/24/solid'
+import { onMounted } from 'vue'
+import { LanguageIcon, SwatchIcon } from '@heroicons/vue/24/solid'
+import { themeChange } from 'theme-change'
 import { GitHubIcon } from '@/components/icons'
 import { updateI18nLocale } from '@/locales'
+
+const themes = [
+  'light',
+  'dark',
+  'cupcake',
+  'bumblebee',
+  'emerald',
+  'corporate',
+  'synthwave',
+  'retro',
+  'cyberpunk',
+  'valentine',
+  'halloween',
+  'garden',
+  'forest',
+  'aqua',
+  'lofi',
+  'pastel',
+  'fantasy',
+  'wireframe',
+  'black',
+  'luxury',
+  'dracula',
+  'cmyk',
+  'autumn',
+  'business',
+  'acid',
+  'lemonade',
+  'night',
+  'coffee',
+  'winter'
+]
+
+onMounted(() => {
+  themeChange(false)
+})
 </script>
 
 <template>
@@ -11,17 +49,54 @@ import { updateI18nLocale } from '@/locales'
     </div>
     <div class="flex-1 flex justify-end px-2">
       <div class="dropdown dropdown-end">
+        <div class="tooltip tooltip-left" :data-tip="$t('Change Theme')">
+          <label class="btn btn-ghost" tabindex="0">
+            <SwatchIcon class="h-6 w-6" />
+          </label>
+        </div>
+        <ul
+          class="dropdown-content max-h-96 h-[70vh] w-52 overflow-y-auto p-2 mt-4 bg-base-200 text-base-content shadow grid gap-2"
+        >
+          <li v-for="theme in themes" :key="theme">
+            <button
+              class="w-full rounded-md outline outline-2"
+              data-act-class="outline"
+              :data-set-theme="theme"
+            >
+              <div
+                class="w-full rounded-md bg-base-100 text-base-content font-sans cursor-pointer"
+                :data-theme="theme"
+              >
+                <div class="grid grid-cols-5 grid-rows-3">
+                  <div
+                    class="col-span-5 row-span-3 row-start-1 flex gap-1 py-3 px-4"
+                  >
+                    <div class="flex-grow text-left text-sm font-bold">
+                      {{ theme }}
+                    </div>
+                    <div class="flex flex-shrink-0 flex-wrap gap-1">
+                      <div class="bg-primary w-2 rounded" />
+                      <div class="bg-secondary w-2 rounded" />
+                      <div class="bg-accent w-2 rounded" />
+                      <div class="bg-neutral w-2 rounded" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </button>
+          </li>
+        </ul>
+      </div>
+      <div class="dropdown dropdown-end">
         <div class="tooltip tooltip-left" :data-tip="$t('Change Language')">
           <label class="btn btn-ghost" tabindex="0">
             <LanguageIcon class="h-6 w-6" />
           </label>
         </div>
-        <ul class="dropdown-content menu w-52 p-2 mt-3 bg-base-100 shadow">
-          <li
-            v-for="locale in $i18n.availableLocales"
-            :key="`locale-${locale}`"
-            :value="locale"
-          >
+        <ul
+          class="dropdown-content menu w-52 p-2 mt-4 bg-base-200 text-base-content shadow gap-1"
+        >
+          <li v-for="locale in $i18n.availableLocales" :key="locale">
             <div
               class="btn btn-ghost"
               :class="{ active: locale === $i18n.locale }"
